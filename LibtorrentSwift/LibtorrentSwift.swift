4a9ea715e15c6ab11b1bea589632049690ed2fd0
Victor <22769960+schuch-v@users.noreply.github.com>
2020-06-20 20:27:05 +0200

    added xcode project

---
+++ LibtorrentSwift/LibtorrentSwift.swift
@@ -0,0 +1,11 @@
+//
+//  LibtorrentSwift.swift
+//  LibtorrentSwift
+//
+//  Created by Victor Schuchmann on 20/06/2020.
+//  Copyright © 2020 Victor Schuchmann. All rights reserved.
+//
+
+class LibtorrentSwift {
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 libtorrent wrapper in swift
 
 ⚠️ boost/tools/build -> develop branch
+
+combine .a libs -> https://stackoverflow.com/questions/28057985/how-to-merge-two-a-files-which-is-built-for-armv7-and-i386
