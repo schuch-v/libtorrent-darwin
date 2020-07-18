16e3784c099bbc814c56dbf2532a1b9a559c7e5a
Victor <22769960+schuch-v@users.noreply.github.com>
2020-07-18 22:30:53 +0200

    build is completing

--- build_libtorrent.sh
+++ build_libtorrent.sh
@@ -1,20 +1,48 @@
 #!/bin/sh
 
-LIBTORRENT_ROOT=$(pwd)/libtorrent
-BOOST_ROOT=$(pwd)/boost
+LIBTORRENT_SWIFT_ROOT=$(pwd)
+LIBTORRENT_ROOT=$LIBTORRENT_SWIFT_ROOT/libtorrent
+BOOST_ROOT=$LIBTORRENT_SWIFT_ROOT/boost
 BOOST_BUILD_ROOT=$BOOST_ROOT/tools/build
 BOOST_BUILD_PATH=$BOOST_BUILD_ROOT/b2
 
 export BOOST_ROOT
 export BOOST_BUILD_PATH
 
+# Boost Build
+
+echo "💬 cd ${BOOST_BUILD_ROOT}"
 cd $BOOST_BUILD_ROOT
+echo "💬 build b2"
 ./bootstrap.sh
+echo "💬 b2 build is DONE !"
 
+echo "💬 writing boost-build.jam"
+echo "boost-build ${BOOST_BUILD_ROOT}/src ;" > $LIBTORRENT_SWIFT_ROOT/boost-build.jam
+echo "💬 writing boost-build.jam DONE !"
+
+# Boost Headers
+
+echo "💬 cd ${BOOST_ROOT}"
 cd $BOOST_ROOT
-$BOOST_BUILD_PATH headers
 
-# Build libtorrent
+echo "💬 build boost headers for iPhone"
+$BOOST_BUILD_PATH headers cxxstd=14 --user-config=../user-config.jam toolset=darwin-iphone
+echo "💬 build boost headers for iPhone DONE !"
+
+echo "💬 build boost headers for Simulator"
+$BOOST_BUILD_PATH headers cxxstd=14 --user-config=../user-config.jam toolset=darwin-iphonesimulator
+echo "💬 build boost headers for Simulator DONE !"
+
+# Libtorrent
+
+echo "💬 cd ${LIBTORRENT_ROOT}"
 cd $LIBTORRENT_ROOT
-$BOOST_BUILD_PATH $BOOST_BUILD_TOOLSET_IPHONE cxxstd=14 link=static --user-config=../user-config.jam toolset=darwin-iphone
-$BOOST_BUILD_PATH $BOOST_BUILD_TOOLSET_SIMULATOR cxxstd=14 link=static --user-config=../user-config.jam toolset=darwin-iphonesimulator
+
+echo "💬 build libtorrent for iPhone"
+$BOOST_BUILD_PATH cxxstd=14 link=static --user-config=../user-config.jam toolset=darwin-iphone
+echo "💬 build libtorrent for iPhone DONE !"
+
+echo "💬 build libtorrent for Simulator"
+$BOOST_BUILD_PATH cxxstd=14 link=static --user-config=../user-config.jam toolset=darwin-iphonesimulator
+echo "💬 build libtorrent for Simulator DONE !"
