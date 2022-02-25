fc01e7d20b5f24e2abbba1447534cc311e182a38
Victor Schuchmann <victor.schuchmann@frichti.co>
2022-02-25 18:28:30 +0100

    build fat lib & xcframework

--- build.sh
+++ build.sh
@@ -123,14 +123,60 @@ build_libtorrent_all_sdk_and_archs() {
     build_libtorrent darwin-mac_x86_64 $MACOS_SDK
 }
 
+create_xcframework() {
+    cd $LIBTORRENT_DARWIN_ROOT
+        
+    local APPLETV_ARM64="$LIBTORRENT_ROOT/bin/darwin-appletv_arm64/release/cxxstd-14-iso/link-static/threading-multi/visibility-hidden/libtorrent-rasterbar.a"
+    local APPLETVSIMULATOR_ARM64="$LIBTORRENT_ROOT/bin/darwin-appletvsimulator_arm64/release/cxxstd-14-iso/link-static/threading-multi/visibility-hidden/libtorrent-rasterbar.a"
+    local APPLETVSIMULATOR_X86_64="$LIBTORRENT_ROOT/bin/darwin-appletvsimulator_x86_64/release/cxxstd-14-iso/link-static/threading-multi/visibility-hidden/libtorrent-rasterbar.a"
+    local APPLETVSIMULATOR_ARM64_X86_64="$LIBTORRENT_ROOT/bin/darwin-appletvsimulator_arm64_x86_64/release/cxxstd-14-iso/link-static/threading-multi/visibility-hidden/libtorrent-rasterbar.a"
+
+    local IPHONE_ARM64="$LIBTORRENT_ROOT/bin/darwin-iphone_arm64/release/cxxstd-14-iso/link-static/threading-multi/visibility-hidden/libtorrent-rasterbar.a"
+    local IPHONESIMULATOR_ARM64="$LIBTORRENT_ROOT/bin/darwin-iphonesimulator_arm64/release/cxxstd-14-iso/link-static/threading-multi/visibility-hidden/libtorrent-rasterbar.a"
+    local IPHONESIMULATOR_X86_64="$LIBTORRENT_ROOT/bin/darwin-iphonesimulator_x86_64/release/cxxstd-14-iso/link-static/threading-multi/visibility-hidden/libtorrent-rasterbar.a"
+    local IPHONESIMULATOR_ARM64_X86_64="$LIBTORRENT_ROOT/bin/darwin-iphonesimulator_arm64_x86_64/release/cxxstd-14-iso/link-static/threading-multi/visibility-hidden/libtorrent-rasterbar.a"
+
+    local MAC_ARM64="$LIBTORRENT_ROOT/bin/darwin-mac_arm64/release/cxxstd-14-iso/link-static/threading-multi/visibility-hidden/libtorrent-rasterbar.a"
+    local MAC_X86_64="$LIBTORRENT_ROOT/bin/darwin-mac_x86_64/release/cxxstd-14-iso/link-static/threading-multi/visibility-hidden/libtorrent-rasterbar.a"
+    local MAC_ARM64_X86_64="$LIBTORRENT_ROOT/bin/darwin-mac_arm64_x86_64/release/cxxstd-14-iso/link-static/threading-multi/visibility-hidden/libtorrent-rasterbar.a"
+
+    mkdir -p $(dirname $APPLETVSIMULATOR_ARM64_X86_64)
+    mkdir -p $(dirname $IPHONESIMULATOR_ARM64_X86_64)
+    mkdir -p $(dirname $MAC_ARM64_X86_64)
+
+    echo "create fat library for appletvsimulator arm64 & x86-64..."
+    lipo -create $APPLETVSIMULATOR_ARM64 $APPLETVSIMULATOR_X86_64 -output $APPLETVSIMULATOR_ARM64_X86_64
+    echo "create fat library for iphonesimulator arm64 & x86-64..."
+    lipo -create $IPHONESIMULATOR_ARM64 $IPHONESIMULATOR_X86_64 -output $IPHONESIMULATOR_ARM64_X86_64
+    echo "create fat library for mac arm64 & x86-64..."
+    lipo -create $MAC_ARM64 $MAC_X86_64 -output $MAC_ARM64_X86_64
+
+    local INCLUDE="$PWD/libtorrent/include"
+    
+    local XCFRAMEWORK_CREATE="xcodebuild -create-xcframework "
+    
+    XCFRAMEWORK_CREATE+="-library $APPLETV_ARM64 -headers $INCLUDE "
+    XCFRAMEWORK_CREATE+="-library $APPLETVSIMULATOR_ARM64_X86_64 -headers $INCLUDE "
+    XCFRAMEWORK_CREATE+="-library $IPHONE_ARM64 -headers $INCLUDE "
+    XCFRAMEWORK_CREATE+="-library $IPHONESIMULATOR_ARM64_X86_64 -headers $INCLUDE "
+    XCFRAMEWORK_CREATE+="-library $MAC_ARM64_X86_64 -headers $INCLUDE "
+
+    XCFRAMEWORK_CREATE+="-output Libtorrent.xcframework"
+    
+    echo "create xcframework..."
+    $XCFRAMEWORK_CREATE
+}
+
 set -e
 
-#print_paths
+print_paths
 
-#build_openssl_all_sdk_and_archs
+build_openssl_all_sdk_and_archs
 
-#build_boost_b2
+build_boost_b2
 
-#build_boost_all_sdk_and_archs
+build_boost_all_sdk_and_archs
 
 build_libtorrent_all_sdk_and_archs
+
+create_xcframework
